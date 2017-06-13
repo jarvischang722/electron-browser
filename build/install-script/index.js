f2a2e489fee0a814b208304a66248e0addcee46d
john.fu <john.fu@tripleonetech.net>
2017-06-13 11:37:47 +0800

    add proxy urls to default options, write pac file by proxy urls or home urls

--- build/install-script/index.js
+++ build/install-script/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const url = require('url')
 const ncp = require('ncp').ncp
 const rcedit = require('rcedit')
 const asar = require('asar')
@@ -51,6 +52,54 @@ const compiler = (iss, options) => {
     })
 }
 
+const writePacFile = async (optionPath, options) => {
+    const { localPort } = options.proxyOptions
+    let homeUrl = options.proxyUrls || options.homeUrl
+    if (!Array.isArray(homeUrl)) {
+        homeUrl = [homeUrl]
+    }
+    let filterString = ''
+    for (const page of homeUrl) {
+        const pageUrl = url.parse(page)
+        let host = pageUrl.host || pageUrl.path
+        const idx = host.lastIndexOf('.')
+        const suffix = host.slice(idx + 1)
+        if (idx > -1) {
+            host = host.slice(0, idx)
+            const main = host.slice(host.lastIndexOf('.') + 1)
+            filterString += `
+        if (/(?:^|\\.)${main}\\.${suffix}$/gi.test(host)) return "+proxy";`
+        } else {
+            filterString += `
+        if (/(?:^|\\.)${suffix}$/gi.test(host)) return "+proxy";`
+        }
+    }
+    const pac = `var FindProxyForURL = function(init, profiles) {
+    return function(url, host) {
+        "use strict";
+        var result = init, scheme = url.substr(0, url.indexOf(":"));
+        do {
+            result = profiles[result];
+            if (typeof result === "function") result = result(url, host, scheme);
+        } while (typeof result !== "string" || result.charCodeAt(0) === 43);
+        return result;
+    };
+}("+safe", {
+    "+safe": function(url, host, scheme) {
+        "use strict";${filterString}
+        return "DIRECT";
+    },
+    "+proxy": function(url, host, scheme) {
+        "use strict";
+        return "SOCKS5 127.0.0.1:${localPort}; SOCKS 127.0.0.1:${localPort}; DIRECT;";
+    }
+});
+`
+    const pacFile = path.join(optionPath, 'default.pac')
+    fs.writeFileSync(pacFile, pac)
+    await copy(pacFile, 'src/app/config/default.pac')
+}
+
 const run = async (optionPath) => {
     const commonOpt = require('../../src/app/config/common.json')
     const optionFile = path.join(optionPath, 'client.json')
@@ -71,8 +120,8 @@ const run = async (optionPath) => {
     await copy('dist/unpacked/electron.exe', 'dist/unpacked/safety-browser.exe', { clobber: false })
     await rceditSync('dist/unpacked/safety-browser.exe' , rceditOptions)
 
-    if (fs.existsSync(path.join(optionPath, 'default.pac'))) {
-        await copy(path.join(optionPath, 'default.pac'), 'src/app/config/default.pac')
+    if (options.enabledProxy && options.proxyOptions) {
+        await writePacFile(optionPath, options)
     }
 
     await copy(optionFile, 'src/app/config/client.json')
